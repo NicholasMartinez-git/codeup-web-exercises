0d25554df1d21babaacc113a06ad7f15167c2e2a
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-02-02 12:13:22 -0600

    feat: add alert and prompt with response console log message

--- JS/external.js
+++ JS/external.js
@@ -1,1 +1,8 @@
-console.log("Hello from external Javascript")
+"use stricit";
+console.log("Hello from external Javascript");
+
+alert("Welcome to my Website!");
+
+var favoriteColor = prompt("What is your favorite color");
+console.log("Great, " + favoriteColor + " is my favorite too!");
+
