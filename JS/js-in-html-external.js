5083bffe2c97fe084656b44a79ed225c144f4cc6
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-02-02 11:41:27 -0600

    feat: add alert/confirm/prompt

--- JS/js-in-html-external.js
+++ JS/js-in-html-external.js
@@ -3,3 +3,14 @@ console.log("Hello from an external JS File");
 
 var name = " Nicholas";
 console.log("Hello" + name);
+
+//Alert
+alert("Welcome to my site!");
+
+//Confirm
+var confirmLeave = confirm("Are you sure you want to leave? 0.0");
+console.log("confirmLeave " + confirmLeave);
+
+//Prompt
+var favoriteColor = prompt("What is your favorite color?");
+console.log("Your favorite color is " + favoriteColor);
