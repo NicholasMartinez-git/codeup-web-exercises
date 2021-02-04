b90c143a15f382b444491117131a29a5c79c856c
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-02-04 16:28:23 -0600

    feat: added HTML directory and 101 exercise solutions html file

---
+++ JS/101-Exercises-Solutions.js
@@ -0,0 +1,5 @@
+// These are the solutions for all 101 Exercises for glitch.com/edit/#!/slime-toothsome-giraffe?path=101-exercises.js%3A43%3A20
+
+// Exercise 1
+var onMarsRightNow = false;
+console.log(onMarsRightNow);
