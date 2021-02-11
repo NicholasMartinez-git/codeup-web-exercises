3da178bc465ed9c540bd89b206647f4ca0f0ec9a
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-02-11 09:55:13 -0600

    feat: add showMultiplicationTable() solution

--- JS/for_loops.js
+++ JS/for_loops.js
@@ -1,5 +1,13 @@
 "use strict";
 
 (function () {
-
+    function showMultiplicationTable(number) {
+        for (var i = 1; i <= 10; i++) {
+            var result = number * i;
+            console.log(number + " x " + i + " = " + result);
+        }
+        return console.log("Here is the table you requested!");
+    }
+    showMultiplicationTable(7);
 })();
+
