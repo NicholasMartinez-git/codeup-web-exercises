1893118d31cef256b44c49fe8e1e467215d4eff5
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-02-11 16:39:19 -0600

    feat: add decrementByFive() function solving output shown.

--- JS/for_loops.js
+++ JS/for_loops.js
@@ -71,6 +71,37 @@
     }
     numberTriangle();
 
+    // Create a for loop that uses console.log to create the output shown below.
+    //
+    //
+    // 100
+    // 95
+    // 90
+    // 85
+    // 80
+    // 75
+    // 70
+    // 65
+    // 60
+    // 55
+    // 50
+    // 45
+    // 40
+    // 35
+    // 30
+    // 25
+    // 20
+    // 15
+    // 10
+    // 5
+
+    function decrementByFive(input) {
+        for (var i = 100; i > 0; i = i - 5) {
+            console.log(i);
+        }
+    }
+    decrementByFive();
+
 
 })();
 
