cc2f7c0c7ea4652ed342928ad43fd33cbb6fa815
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-02-11 13:50:46 -0600

    feat: add generate10RandomNumbers() to show a for loop console logging 10 random numbers between 20-200 and if the random number is even or odd.

--- JS/for_loops.js
+++ JS/for_loops.js
@@ -9,5 +9,17 @@
         return console.log("Here is the table you requested!");
     }
     showMultiplicationTable(7);
+    
+    function generate10RandomNumbers() {
+        for (var i = 0; i < 10; i++) {
+            var randomNumber = Math.floor((Math.random() * 200) + 20);
+            if (randomNumber % 2 === 0) {
+                console.log(randomNumber + " is even.");
+            } else {
+                console.log(randomNumber + " is odd.");
+            }
+        }
+    }
+    generate10RandomNumbers()
 })();
 
