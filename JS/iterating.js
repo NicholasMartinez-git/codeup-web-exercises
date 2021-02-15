49243bfd7626cc8fe5be672ddc087c3c41fe62b6
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-02-14 22:24:21 -0600

    feat: add first 3 TODO solutions

--- JS/iterating.js
+++ JS/iterating.js
@@ -6,18 +6,24 @@
      * Create an array of 4 people's names and store it in a variable called
      * 'names'.
      */
-
+    var names = ["Nicholas", "Doc", "Blackbeard", "D.Rob"];
+    console.log(names);
     /**
      * TODO:
      * Create a log statement that will log the number of elements in the names
      * array.
      */
+    console.log(names.length);
 
     /**
      * TODO:
      * Create log statements that will print each of the names individually by
      * accessing each element's index.
      */
+    console.log(names[0]);
+    console.log(names[1]);
+    console.log(names[2]);
+    console.log(names[3]);
 
     /**
      * TODO:
