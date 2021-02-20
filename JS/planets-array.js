f45b846c603b84424c67ccd612e1fee86525d76b
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-02-20 00:39:44 -0600

    feat: add planets.unshift('The Sun') and planets.push('Pluto')

--- JS/planets-array.js
+++ JS/planets-array.js
@@ -19,12 +19,15 @@
      */
 
     console.log('Adding "The Sun" to the beginning of the planets array.');
+    planets.unshift("The Sun");
     console.log(planets);
 
     console.log('Adding "Pluto" to the end of the planets array.');
+    planets.push("Pluto");
     console.log(planets);
 
     console.log('Removing "The Sun" from the beginning of the planets array.');
+
     console.log(planets);
 
     console.log('Removing "Pluto" from the end of the planets array.');
