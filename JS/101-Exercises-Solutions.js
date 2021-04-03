b4b80e9094b6148c7863608bfced76cefac0bdf1
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-04-03 13:02:19 -0500

    added 91 of 101 Ex-Sol

--- JS/101-Exercises-Solutions.js
+++ JS/101-Exercises-Solutions.js
@@ -826,3 +826,31 @@ function getPrice(object) {
 function getBookAuthor(object) {
     return object.author;
 }
+
+const books = [
+    {
+        "title": "Genetic Algorithms and Machine Learning for Programmers",
+        "price": 36.99,
+        "author": "Frances Buontempo"
+    },
+    {
+        "title": "The Visual Display of Quantitative Information",
+        "price": 38.00,
+        "author": "Edward Tufte"
+    },
+    {
+        "title": "Practical Object-Oriented Design",
+        "author": "Sandi Metz",
+        "price": 30.47
+    },
+    {
+        "title": "Weapons of Math Destruction",
+        "author": "Cathy O'Neil",
+        "price": 17.44
+    }
+]
+
+// Exercise 91
+function getNumberOfBooks(books) {
+    return books.length;
+}
