7bd470ac1602aa6d051de8f67fc9054ec71660db
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-04-06 11:46:24 -0500

    added instructed .click & bg-color to h1 element selector

--- JS/jquery_exercises.js
+++ JS/jquery_exercises.js
@@ -1,11 +1,7 @@
 "use strict";
 
 $(document).ready(function() {
-
-    $('li').css('font-size', '20px');
-
-    $('h1, p, li').css('background-color', 'yellow');
-
-    var contents = $('h1').html();
-    alert(contents);
+    $("h1").click(function (e) {
+       $("h1").css("background", "rebeccapurple");
+    });
 });
