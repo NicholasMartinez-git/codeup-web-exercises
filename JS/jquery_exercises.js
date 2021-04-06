df4ac671923a394ba5d32bed5b16470fbd276f40
Nicholas Martinez <nicholas.martinez212@gmail.com>
2021-04-06 11:48:58 -0500

    added instructed .dblclick & font-size to <p> element selector

--- JS/jquery_exercises.js
+++ JS/jquery_exercises.js
@@ -1,7 +1,13 @@
 "use strict";
 
 $(document).ready(function() {
-    $("h1").click(function (e) {
+
+    $("h1").click(function () {
        $("h1").css("background", "rebeccapurple");
     });
+
+    $("p").dblclick(function (e) {
+        $(this).css("font-size", "18px");
+    });
+
 });
